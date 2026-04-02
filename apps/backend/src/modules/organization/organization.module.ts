c65a4b57389f1fc9e0b71d091e66e0c9c43c8c87
MI-OM <omfmail@aol.com>
2026-04-02 22:47:17 +0100

    fix: import StorageModule into OrganizationModule

--- apps/backend/src/modules/organization/organization.module.ts
+++ apps/backend/src/modules/organization/organization.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Organization } from './entities/organization.entity';
 import { OrganizationService } from './services/organization.service';
 import { OrganizationController } from './controllers/organization.controller';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Organization])],
+  imports: [TypeOrmModule.forFeature([Organization]), StorageModule],
   controllers: [OrganizationController],
   providers: [OrganizationService],
   exports: [OrganizationService],
